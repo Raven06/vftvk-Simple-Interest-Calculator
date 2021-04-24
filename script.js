555306eae9e37128ac0c31cbc28691099eceb47d
Raven06 <aniketaniket@hotmail.com>
2021-04-24 06:50:19 +0000

    Complete

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-function compute()
-{
+function compute(){
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     var interest = principal * years * rate / 100;
     var year = new Date().getFullYear()+parseInt(years);
+    if(principal<0){
+        //checks if amount is positive
+        alert("Enter a positive nummber")
+    }
+    else{
+        document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>"
+    }
+    
 }
 
 function updateRate(){
