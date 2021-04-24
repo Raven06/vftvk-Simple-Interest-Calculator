d08ea602b0d87517427ffadcbc17d55c14a68648
Raven06 <aniketaniket@hotmail.com>
2021-04-24 08:40:27 +0000

    added highlight

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ function compute(){
     var years = document.getElementById("years").value;
     var interest = principal * years * rate / 100;
     var year = new Date().getFullYear()+parseInt(years);
-    if(principal<0){
+    if(principal<=0){
         //checks if amount is positive
         alert("Enter a positive nummber")
     }
     else{
-        document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>"
+        document.getElementById("result").innerHTML="If you deposit  <span class='simple-highlight'>"+principal+"</span>,\<br\>at an interest rate of <span class='simple-highlight'>"+rate+"%</span>\<br\>You will receive an amount of <span class='simple-highlight'>"+interest+"</span>,\<br\>in the year <span class='simple-highlight'>"+year+"</span>\<br\>"
     }
     
 }
